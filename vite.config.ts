16636e579403074cd666b87b121446dbe357e247
Machadomq <gbrielmq1213@gmail.com>
2025-09-25 17:12:35 -0300

    Fix GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+    plugins: [react()],
+    base: '/portfolio/',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets'
+    }
 })
