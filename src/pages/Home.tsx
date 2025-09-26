8b79d2edec245229abd3500d7e7dba7d2d469c26
Machadomq <gbrielmq1213@gmail.com>
2025-09-26 14:43:55 -0300

    Remove animated scroll indicator arrow - Clean up UI by removing bouncing arrow animation - Improve visual simplicity of homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -168,22 +168,6 @@ const Home = () => {
                         </motion.div>
                     </motion.div>
                 </div>
-
-                {/* Scroll Indicator */}
-                <motion.div
-                    initial={{ opacity: 0, y: 20 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ delay: 1, duration: 0.5 }}
-                    className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
-                >
-                    <motion.div
-                        animate={{ y: [0, 10, 0] }}
-                        transition={{ duration: 2, repeat: Infinity }}
-                        className="text-gray-400 dark:text-gray-500"
-                    >
-                        
-                    </motion.div>
-                </motion.div>
             </section>
 
             {/* Skills Preview Section */}
